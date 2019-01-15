4c6f8570bfc637e0f24077b4566616d96a7ea466
Maarten van der Meulen <mjvandermeulen@gmail.com>
2019-01-15 00:00:58 -0600

    added power movement Adafruit basic powertail test

---
+++ rpi_gpio_projects_and_tests/power_movement_2019.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Adafruit
+# https: // learn.adafruit.com/adafruits-raspberry-pi-lesson-13-power-control
+
+import time
+import RPi.GPIO as io
+
+io.setmode(io.BCM)
+
+pir_pin = 24
+power_pin = 23
+
+io.setup(pir_pin, io.IN)
+io.setup(power_pin, io.OUT)
+io.output(power_pin, False)
+
+while True:
+    if io.input(pir_pin):
+        print("POWER ON")
+        io.output(power_pin, True)
+        time.sleep(2)
+        print("POWER OFF")
+        io.output(power_pin, False)
+        time.sleep(5)
+    time.sleep(1)
