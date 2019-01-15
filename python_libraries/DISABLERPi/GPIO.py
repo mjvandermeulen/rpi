97b7e449f4e6f851b3f37af5def6056ff7eb7641
Maarten van der Meulen <mjvandermeulen@gmail.com>
2019-01-14 23:58:27 -0600

    Added dummy RPi GPIO library, for editing on Mac. See READ_ME.md

---
+++ python_libraries/DISABLERPi/GPIO.py
@@ -0,0 +1,29 @@
+BOARD = 1
+OUT = 1
+IN = 1
+
+BCM = True
+
+
+def setmode(a):
+    print a
+
+
+def setup(a, b):
+    print a
+
+
+def input(a):
+    print a
+
+
+def output(a, b):
+    print a
+
+
+def cleanup():
+    print 'a'
+
+
+def setwarnings(flag):
+    print 'False'
---
+++ python_libraries/DISABLERPi/READ_ME.md
@@ -0,0 +1,9 @@
+# Dummy Module
+
+## Mac
+
+The RPi.GPIO module (library) is only needed when working on a Mac
+
+## R Pi ALERT
+
+Disable this module when running code on the R Pi!
