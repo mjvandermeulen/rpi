4a8f3b93e5add0e4a7ca1f9102ea3c141d37ae55
Maarten van der Meulen <mjvandermeulen@gmail.com>
2019-01-25 20:13:12 -0600

    improved confusion causing powertail on off script

--- adventures/power_tail/turn_power_tail_on_or_off.py
+++ adventures/power_tail/turn_power_tail_on_or_off.py
@@ -5,23 +5,26 @@ import sys
 
 ### settings ###
 pin_mode = "BCM"
-power_pin = 23
+power_pin = 23  # default
 
-# create instance of PowerTail
-power = powertail.PowerTail(pin_mode, power_pin, False)
 if len(sys.argv) > 1 and 'n' in sys.argv[1]:
     power_on = True
+    if len(sys.argv) > 2:
+        try:
+            boo = int(sys.argv[2])
+        except:
+            raise Exception('second argument (PIN) needs to be an integer')
 else:
     power_on = False
 
 print()
-print()
 
+# create instance of PowerTail
+power = powertail.PowerTail(pin_mode, power_pin, power_on)
+
+print()
 if power_on:
     print("POWER ON")
 else:
     print("POWER OFF")
-power.turn(power_on)
-
-print()
 print()
